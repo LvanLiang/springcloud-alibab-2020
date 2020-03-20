04e45d083e4d6859cc44c103eee064cbd69c6570
liangxp <liangxp0@126.com>
2020-03-20 17:36:25 +0800

    消费者使用zookeeper作为注册中心，将服务提供者注册进去

---
+++ consumer-order-zk/src/main/java/com/liang/OrderZkApplication.java
@@ -0,0 +1,15 @@
+package com.liang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Liangxp
+ * @date 2020/3/20 17:29
+ */
+@SpringBootApplication
+public class OrderZkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZkApplication.class, args);
+    }
+}
---
+++ consumer-order-zk/src/main/java/com/liang/config/ApplicationContextConfig.java
@@ -0,0 +1,20 @@
+package com.liang.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Liangxp
+ * @date 2020/3/20 15:23
+ */
+@Configuration
+public class ApplicationContextConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
