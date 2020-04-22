e3660e3a95591dd0529dc1098c4291ba04b12b54
Liangxp <liangxp0@126.com>
2020-04-22 22:41:15 +0800

    spring cloud stream消息分组消费和持久化

--- stream-rabbitmq-consumer/src/main/java/com/liang/StreamMqConsumerApplication.java
+++ stream-rabbitmq-consumer/src/main/java/com/liang/StreamMqConsumerApplication.java
@@ -1,8 +1,15 @@
 package com.liang;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Liangxp
  * @date 2020/4/10 17:23
  */
+@SpringBootApplication
 public class StreamMqConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMqConsumerApplication.class, args);
+    }
 }
