5137031f1f1c98926876cfe031ba90cafe00e5e8
liangxp <liangxp0@126.com>
2020-05-09 15:26:41 +0800

    在其他机器上使用seata实现分布式事物

--- seata-samples/sca-account-provider/src/main/java/com/liang/service/AccountServiceImpl.java
+++ seata-samples/sca-account-provider/src/main/java/com/liang/service/AccountServiceImpl.java
@@ -7,7 +7,6 @@ import org.apache.dubbo.config.annotation.Service;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author Liangxp
@@ -24,7 +23,6 @@ public class AccountServiceImpl implements AccountService {
         log.info("------->account-service中扣减账户余额开始");
         //模拟超时异常，全局事务回滚
         //暂停几秒钟线程
-        try { TimeUnit.SECONDS.sleep(20); } catch (InterruptedException e) { e.printStackTrace(); }
         accountMapper.decrease(userId, money);
         log.info("------->account-service中扣减账户余额结束");
     }
